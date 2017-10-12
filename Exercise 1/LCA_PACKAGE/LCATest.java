2c4a375a64f0cf780ded1feded14b5589d0a1455
oboyle-mikey <oboylemi@tcd.ie>
2017-10-12 18:08:54 +0100

    Commit 4: Test Initialisation
    Setting up an initial set of data in the test suite.

--- Exercise 1/LCA_PACKAGE/LCATest.java
+++ Exercise 1/LCA_PACKAGE/LCATest.java
@@ -9,21 +9,46 @@ public class LCATest {
 	//Test 1: Empty Tree Case
 	@Test 
 	public void test1() {
-		
+		BST test = new BST();
+		test.put(10);
+		test.put(2);
+		test.put(3);
+		test.put(41);
+		test.put(12);
 	}
 	
 	//Test 2: Two nodes with separate node as common ancestor
 	@Test
 	public void test2(){
-		
+		BST test = new BST();
+		test.put(10);
+		test.put(2);
+		test.put(3);
+		test.put(41);
+		test.put(12);
 	}
 	
 	//Test 3: Two nodes with one node as the common ancestor
 	@Test 
 	public void test3(){
-		
+		BST test = new BST();
+		test.put(10);
+		test.put(2);
+		test.put(3);
+		test.put(41);
+		test.put(12);
 	}
 	
+	//Test 4: Tree without two key references contained in the data
+	@Test
+	public void test4(){
+		BST test = new BST();
+		test.put(10);
+		test.put(2);
+		test.put(3);
+		test.put(41);
+		test.put(12);
+	}
 	//Test Other Cases
 
 }
