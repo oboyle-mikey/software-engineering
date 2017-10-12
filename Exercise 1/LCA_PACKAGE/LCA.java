0c6cf3d559fdbc186a1d9d6b28fcf239432310fa
oboyle-mikey <oboylemi@tcd.ie>
2017-10-12 18:15:18 +0100

    Commit 6: Moving function
    Moving function to proper area.

--- Exercise 1/LCA_PACKAGE/LCA.java
+++ Exercise 1/LCA_PACKAGE/LCA.java
@@ -2,13 +2,5 @@ package LCA_PACKAGE;
 
 public class LCA {
 
-	public static BST initializeData(){
-		BST test = new BST();
-		test.put(10);
-		test.put(2);
-		test.put(3);
-		test.put(41);
-		test.put(12);
-		return test;
-	}
+	
 }
--- Exercise 1/LCA_PACKAGE/LCATest.java
+++ Exercise 1/LCA_PACKAGE/LCATest.java
@@ -9,27 +9,37 @@ public class LCATest {
 	//Test 1: Empty Tree Case
 	@Test 
 	public void test1() {
-		BST test = LCA.initializeData();
+		BST test = initializeData();
 	}
 	
 	//Test 2: Two nodes with separate node as common ancestor
 	@Test
 	public void test2(){
-		BST test = LCA.initializeData();
+		BST test = initializeData();
 	}
 	
 	//Test 3: Two nodes with one node as the common ancestor
 	@Test 
 	public void test3(){
-		BST test = LCA.initializeData();
+		BST test = initializeData();
 	}
 	
 	//Test 4: Tree without two key references contained in the data
 	@Test
 	public void test4(){
-		BST test = LCA.initializeData();
+		BST test = initializeData();
 	}
 	
+	//Method to standardise initialisation of BST
+	public static BST initializeData(){
+		BST test = new BST();
+		test.put(10);
+		test.put(2);
+		test.put(3);
+		test.put(41);
+		test.put(12);
+		return test;
+	}
 	//Test Other Cases
 
 }
