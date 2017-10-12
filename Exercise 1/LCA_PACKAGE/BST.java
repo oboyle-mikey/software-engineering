2542f26fdfada3c3d04b6ffb84c57364b49fcd2d
oboyle-mikey <oboylemi@tcd.ie>
2017-10-12 17:47:18 +0100

    Commit 3: BST Put Method
    Method for implementing put in the data structure. With this function tested, I can create test data in order to procede with testing expection function behaviour.

--- Exercise 1/LCA_PACKAGE/BST.java
+++ Exercise 1/LCA_PACKAGE/BST.java
@@ -5,7 +5,7 @@ public class BST <Key extends Comparable<Key>>{
 	//Private class representing the nodes in the binary tree, the ancestry will be 
 	//based off the parameters passed representing keys
 
-	private class Node{
+	public class Node{
 		Node left, right;
 		Key key;
 		
@@ -16,7 +16,7 @@ public class BST <Key extends Comparable<Key>>{
 	
 	//Root node that will be referenced in the tree as the node with no parent
 	
-	private Node root;
+	public Node root;
 	
 	//Default constructor - NB *** possible default to test data in construct???
 	
@@ -24,7 +24,25 @@ public class BST <Key extends Comparable<Key>>{
 		
 	}
 	
+	//Put method for inserting nodes into the tree, recursive using compare to implement 
+	// an ordered binary search tree
 	
+	public void put(Key key){
+		root = put(root, key);
+	}
+
+	private Node put(Node x, Key key){
+		if(x==null){
+			return new Node(key);
+		}
+		int cmp = key.compareTo(x.key);
+		if(cmp<0){
+			x.left = put(x.left, key);
+		}else if(cmp>0){
+			x.right = put(x.right, key);
+		}
+		return x;
+	}
 	
 	
 	
--- Exercise 1/LCA_PACKAGE/BSTTest.java
+++ Exercise 1/LCA_PACKAGE/BSTTest.java
@@ -5,10 +5,30 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BSTTest {
+	
+	//Test constructor
+	@Test
+	public void testConstruct(){
+		BST test = new BST();
+	}
+	
+	//Testing put method
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPut() {
+		BST test = new BST();
+		test.put(10);
+		test.put(2);
+		test.put(3);
+		test.put(41);
+		test.put(12);
+		test.put(10);
+		assertEquals(test.root.key, 10);
+		assertEquals(test.root.right.key, 41);
+		assertEquals(test.root.left.key, 2);
+		assertEquals(test.root.right.left.key, 12);
+		assertEquals(test.root.left.right.key, 3);
+		
 	}
 
 }
