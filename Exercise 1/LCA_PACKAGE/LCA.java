1525dfe076a2eec612d000abcbf2c5567f06eceb
oboyle-mikey <oboylemi@tcd.ie>
2017-10-17 10:01:27 +0100

    Commit 9: Get Method
    Get method to search the binary tree for a given key as parameterised by the method.

--- Exercise 1/LCA_PACKAGE/LCA.java
+++ Exercise 1/LCA_PACKAGE/LCA.java
@@ -3,49 +3,61 @@ package LCA_PACKAGE;
 import LCA_PACKAGE.BST.Node;
 
 public class LCA <Key extends Comparable<Key>>{
-	
+
 	//Private class representing the nodes in the binary tree, the ancestry will be 
-		//based off the parameters passed representing keys
+	//based off the parameters passed representing keys
 
-		public class Node{
-			Node left, right;
-			Key key;
-			
-			public Node(Key key){
-				this.key = key;
-			}
-		}
-		
-		//Root node that will be referenced in the tree as the node with no parent
-		
-		public Node root;
-		
-		//Default constructor - NB *** possible default to test data in construct???
-		
-		public LCA(){
-			
-		}
-		
-		//Put method for inserting nodes into the tree, recursive using compare to implement 
-		// an ordered binary search tree
-		
-		public void put(Key key){
-			root = put(root, key);
-		}
+	public class Node{
+		Node left, right;
+		Key key;
 
-		private Node put(Node x, Key key){
-			if(x==null){
-				return new Node(key);
-			}
-			int cmp = key.compareTo(x.key);
-			if(cmp<0){
-				x.left = put(x.left, key);
-			}else if(cmp>0){
-				x.right = put(x.right, key);
-			}
-			return x;
+		public Node(Key key){
+			this.key = key;
 		}
-		
+	}
+
+	//Root node that will be referenced in the tree as the node with no parent
+
+	public Node root;
+
+	//Default constructor - NB *** possible default to test data in construct???
+
+	public LCA(){
+
+	}
+
+	//Put method for inserting nodes into the tree, recursive using compare to implement 
+	// an ordered binary search tree
+
+	public void put(Key key){
+		root = put(root, key);
+	}
+
+	private Node put(Node x, Key key){
+		if(x==null){
+			return new Node(key);
+		}
+		int cmp = key.compareTo(x.key);
+		if(cmp<0){
+			x.left = put(x.left, key);
+		}else if(cmp>0){
+			x.right = put(x.right, key);
+		}
+		return x;
+	}
+
+	//Get method to search a tree for a given key value
+	
+	public boolean get(Key key) { return get(root, key); }
+
+	private boolean get(Node x, Key key) {
+		if (x == null) return false;
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) return get(x.left, key);
+		else if (cmp > 0) return get(x.right, key);
+		else              return true;
+	}
+
 
 	public int lowestCommonAncestor(Key key1, Key key2){
 		return 0;
--- Exercise 1/LCA_PACKAGE/LCATest.java
+++ Exercise 1/LCA_PACKAGE/LCATest.java
@@ -5,23 +5,23 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LCATest{
-	
+
 	//Test 1: Empty Tree Case
 	@Test 
 	public void test1() {
 		LCA test = new LCA();
-		
+
 	}
-	
+
 	//Test 2: Two nodes with separate node as common ancestor
 	@Test
 	public void test2(){
 		LCA test = initializeData();
 		int t = test.lowestCommonAncestor(3, 12);
 		assertEquals(t, 10);
-		
+
 	}
-	
+
 	//Test 3: Two nodes with one node as the common ancestor
 	@Test 
 	public void test3(){
@@ -29,7 +29,7 @@ public class LCATest{
 		int t = test.lowestCommonAncestor(2, 3);
 		assertEquals(t, 2);
 	}
-	
+
 	//Test 4: Tree without two key references contained in the data
 	@Test
 	public void test4(){
@@ -37,9 +37,9 @@ public class LCATest{
 		int t = test.lowestCommonAncestor(13, 120);
 		assertEquals(t, -1);
 	}
-	
+
 	//Test Other Cases
-	
+
 	//Method to standardise initialisation of BST
 	public static LCA initializeData(){
 		LCA test = new LCA();
@@ -50,34 +50,43 @@ public class LCATest{
 		test.put(12);
 		return test;
 	}
-	
-	//Test Data Structure Methods 
-	
-	//Test constructor
-		@Test
-		public void testConstruct(){
-			LCA test = new LCA();
-		}
-		
-		//Testing put method
 
-		@Test
-		public void testPut() {
-			LCA test = new LCA();
-			test.put(10);
-			test.put(2);
-			test.put(3);
-			test.put(41);
-			test.put(12);
-			test.put(10);
-			assertEquals(test.root.key, 10);
-			assertEquals(test.root.right.key, 41);
-			assertEquals(test.root.left.key, 2);
-			assertEquals(test.root.right.left.key, 12);
-			assertEquals(test.root.left.right.key, 3);
-			
-		}
+	//Test Data Structure Methods 
+
+	//Test constructor
+	@Test
+	public void testConstruct(){
+		LCA test = new LCA();
+	}
+
+	//Testing put method
+
+	@Test
+	public void testPut() {
+		LCA test = new LCA();
+		test.put(10);
+		test.put(2);
+		test.put(3);
+		test.put(41);
+		test.put(12);
+		test.put(10);
+		assertEquals(test.root.key, 10);
+		assertEquals(test.root.right.key, 41);
+		assertEquals(test.root.left.key, 2);
+		assertEquals(test.root.right.left.key, 12);
+		assertEquals(test.root.left.right.key, 3);
+
+	}
 	
-	
+	@Test 
+	public void testGet(){
+		LCA test = initializeData();
+		boolean t1 = test.get(10);
+		boolean t2 = test.get(123);
+		assertEquals(t1, true);
+		assertEquals(t2, false);
+	}
+
+
 
 }
