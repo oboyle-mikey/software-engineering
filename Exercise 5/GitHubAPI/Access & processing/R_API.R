57a4158ef46bccaee16e2a02bc8c7f21e86842c8
oboyle-mikey <oboylemi@tcd.ie>
2017-12-11 18:34:37 +0000

    Update Auth
    Including my token into the requests to avoid rate limiting

--- Exercise 5/GitHubAPI/Access & processing/R_API.R
+++ Exercise 5/GitHubAPI/Access & processing/R_API.R
@@ -3,6 +3,7 @@
 library(jsonlite) 
 library(httpuv)
 library(httr)
+library(plotly)
 
 #Authenticte Access
 
@@ -20,40 +21,37 @@ token = config(token = access_token)
 #Initial State
 
 nameState = c('oboyle-mikey')
-myProfile = fromJSON("https://api.github.com/users/oboyle-mikey")
-myFollowers = fromJSON("https://api.github.com/users/oboyle-mikey/followers")
-userNames = c(myFollowers$login)
+myProfile_JSON = content(GET("https://api.github.com/users/oboyle-mikey", token))
+myFollowers_JSON = content(GET("https://api.github.com/users/oboyle-mikey/followers", token))
+myFollowers_R = jsonlite::fromJSON(jsonlite::toJSON(myFollowers_JSON))
+userNames = myFollowers_R$login
 
+#Creating Dataframe
+nameState = c()
+follow = c()
+
+#My Followers
 for(i in 1:length(userNames)){
   nameState = c(nameState, paste0("oboyle-mikey.",userNames[i]))
-  i = i+1
+  theirProfile_JSON = content(GET(paste0("https://api.github.com/users/",userNames[i]), token))
+  theirProfile_R = jsonlite::fromJSON(jsonlite::toJSON(theirProfile_JSON))
+  follow = c(follow, theirProfile_R$followers)
 }
-network = userNames
+
 
 
 #My Followers Followers
 for(i in 1:length(userNames)){
-  theirFollowers = fromJSON(paste0("https://api.github.com/users/",userNames[i], "/followers"))
-  theirUserNames = c(theirFollowers$login)
+  theirFollowers_JSON = content(GET((paste0("https://api.github.com/users/",userNames[i], "/followers"))))
+  theirFollowers_R = jsonlite::fromJSON(jsonlite::toJSON(theirFollowers_JSON))
+  theirUserNames = c(theirFollowers_R$login)
   for(j in 1:length(theirUserNames)){
-    nameState = c(nameState, paste0("oboyle-mikey.",userNames[i],".", theirUserNames[j]))
-    j = j+1
+      nameState = c(nameState, paste0("oboyle-mikey.",userNames[i],".", theirUserNames[j]))
+      theirFollowersProfile_JSON = content(GET(paste0("https://api.github.com/users/",theirUserNames[j]), token))
+      theirFollowersProfile_R = jsonlite::fromJSON(jsonlite::toJSON(theirFollowersProfile_JSON))
+      follow = c(follow, theirFollowersProfile_R$followers)
   }
-  network = c(network, theirUserNames)
-  i = i+1
 }
-
-uniqueNet = unique(network)
-
-#Their Followers Followers
-for(i in 1:length(uniqueNet)){
-  theirFollowers = fromJSON(paste0("https://api.github.com/users/",uniqueNet[i], "/followers"))
-  theirUserNames = c(theirFollowers$login)
-  network = c(network, theirUserNames)
-  i = i+1
-}
-
-uniqueNet = unique(network)
 
 
 #For each user we need the following format to import into d3.js Hierarchical Edge Building
@@ -67,14 +65,5 @@ uniqueNet = unique(network)
 #     etc..
 # ]
 
-
-for(i in 1:uniqueNet(length)){
-  name = paste0("oboyle-mikey.",uniqueNet[1])
-  theirProfile = fromJSON(paste0("https://api.github.com/users/",name))
-  followers = theirProfile$followers
-  theirFollowers = fromJSON(paste0("https://api.github.com/users/",uniqueNet[1], "/followers"))
-  theirUserNames = c(theirFollowers$login)
-  write = c(' name : ', name)
-}
 
 
